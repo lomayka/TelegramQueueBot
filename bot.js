59638bdf741aad89e3b3b06519239b712ffcf450
Triodin <ufolkin1@gmail.com>
2017-07-07 18:39:28 +0300

    Change public token to environment variable

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '411029336:AAGul47pNcaV5jM178h2iPITASwnFQ9XrdA';
+//const token = '';
+const token = process.env.TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
