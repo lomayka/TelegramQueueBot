b3348f6e433b200cdc50fbea9c70b95d23bb7b52
Triodin <ufolkin1@gmail.com>
2017-07-07 18:47:31 +0300

    Complete

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-//const token = '';
 const token = process.env.TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
